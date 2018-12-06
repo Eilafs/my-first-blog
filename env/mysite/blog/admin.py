3e3648994d7af68ad5cf286b19ccc08ca56f3b3c
Eilaf <eilaf.alshamilan@grad.ku.edu.kw>
2018-12-06 22:26:18 +0300

    My Django Girls app, first commit

---
+++ env/mysite/blog/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Post
+ # THIS IS A COMMENT
+admin.site.register(Post)
---
+++ env/mysite/blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Post (models.Model):
+	title = models.CharField(max_length=250)
+	text = models.TextField()
+	created_date = models.DateTimeField(default=timezone.now())
+	published_date = models.DateTimeField(blank=True, null= True)
+
+	def __str__(self):
+		return self.title
