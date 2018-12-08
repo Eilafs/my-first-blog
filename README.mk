d3679a19279b920549940594aa09b14daa80b764
Eilafs <45670553+Eilafs@users.noreply.github.com>
2018-12-08 17:24:05 +0300

    Create README.mk

---
+++ README.mk
@@ -0,0 +1,7 @@
+#Hi!!! 
+
+**Out of respect for humanity … **
+
+To increase the awareness in any public opinion case. In this blog I will showcase some problem that went viral, then me and my team will write our thoughts about them. We welcome you to write your thoughts as well in the comment section. Let’s share our respect for others and try to do butter in life. Because once life is affected by all the surrounded public case and all of us have their own opinion about them. Example can be about humans that lost their respect to one another, we will use them as a reminder to behave right. We will not point fingers, we will only stat our thoughts of the situation.
+That said, please enjoy your readings, and don’t forget to share your thoughts…
+  
